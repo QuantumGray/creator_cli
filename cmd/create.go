54173b625fedcde9efd954e2baaa70bee7a637f9
Ben Fornefeld <ben.fornefeld@gmail.com>
2020-10-04 22:36:06 +0200

    FIX: Package warning

--- cmd/create.go
+++ cmd/create.go
@@ -1,7 +1,8 @@
+package cmd
+
 /*
 Copyright © 2020 NAME HERE <EMAIL ADDRESS>
 
-hurensohn!
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -15,7 +16,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-package cmd
 
 import (
 	"errors"
