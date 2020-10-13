4114a1bb5e5f8b86fe413a85b1df0bc074d0844f
niklasvh99 <42723242+niklasvh99@users.noreply.github.com>
2020-10-13 16:15:27 +0200

    linter

--- util/validators/validators.go
+++ util/validators/validators.go
@@ -1,15 +1,16 @@
 package validators
 
 import (
-	"github.com/QuantumGray/fluttercreator_cli/typeserrors"
+	typeserrors "fluttercreator/util/typeserrors"
 )
 
+// CreateCommandArgsValidation : validates arguments from create command
 func CreateCommandArgsValidation(args []string) error {
 	if len(args[0]) != 8 {
-		return typeserrors.InvalidArgumentErr
+		return typeserrors.ErrInvalidArgument
 	}
 	if len(args) != 1 {
-		return typeserrors.InvalidArgumentErr
+		return typeserrors.ErrInvalidArgument
 	}
 	return nil
 }
